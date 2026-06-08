3ee043bfe5bd0aeb72dd37dbcb60a7f12dd376cc
roger696969 <roger.aquino.ga@gmail.com>
2026-06-08 12:25:46 -0400

    dimensionar el boton

--- JUGADORES.Designer.cs
+++ JUGADORES.Designer.cs
@@ -37,20 +37,20 @@
             // TxtNom
             // 
             this.TxtNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtNom.Location = new System.Drawing.Point(53, 27);
-            this.TxtNom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TxtNom.Location = new System.Drawing.Point(71, 33);
+            this.TxtNom.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TxtNom.Name = "TxtNom";
-            this.TxtNom.Size = new System.Drawing.Size(154, 28);
+            this.TxtNom.Size = new System.Drawing.Size(204, 34);
             this.TxtNom.TabIndex = 0;
             // 
             // BtnRegistrarJu
             // 
             this.BtnRegistrarJu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnRegistrarJu.BackgroundImage")));
             this.BtnRegistrarJu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.BtnRegistrarJu.Location = new System.Drawing.Point(530, 21);
-            this.BtnRegistrarJu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnRegistrarJu.Location = new System.Drawing.Point(707, 26);
+            this.BtnRegistrarJu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnRegistrarJu.Name = "BtnRegistrarJu";
-            this.BtnRegistrarJu.Size = new System.Drawing.Size(140, 42);
+            this.BtnRegistrarJu.Size = new System.Drawing.Size(187, 52);
             this.BtnRegistrarJu.TabIndex = 1;
             this.BtnRegistrarJu.UseVisualStyleBackColor = true;
             // 
@@ -58,10 +58,10 @@
             // 
             this.BtnEliminarJu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnEliminarJu.BackgroundImage")));
             this.BtnEliminarJu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.BtnEliminarJu.Location = new System.Drawing.Point(546, 103);
-            this.BtnEliminarJu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnEliminarJu.Location = new System.Drawing.Point(728, 127);
+            this.BtnEliminarJu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnEliminarJu.Name = "BtnEliminarJu";
-            this.BtnEliminarJu.Size = new System.Drawing.Size(140, 42);
+            this.BtnEliminarJu.Size = new System.Drawing.Size(187, 52);
             this.BtnEliminarJu.TabIndex = 2;
             this.BtnEliminarJu.UseVisualStyleBackColor = true;
             // 
@@ -69,10 +69,10 @@
             // 
             this.BtnBuscarJu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnBuscarJu.BackgroundImage")));
             this.BtnBuscarJu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.BtnBuscarJu.Location = new System.Drawing.Point(530, 203);
-            this.BtnBuscarJu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnBuscarJu.Location = new System.Drawing.Point(707, 250);
+            this.BtnBuscarJu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnBuscarJu.Name = "BtnBuscarJu";
-            this.BtnBuscarJu.Size = new System.Drawing.Size(140, 42);
+            this.BtnBuscarJu.Size = new System.Drawing.Size(187, 52);
             this.BtnBuscarJu.TabIndex = 3;
             this.BtnBuscarJu.UseVisualStyleBackColor = true;
             // 
@@ -80,10 +80,10 @@
             // 
             this.BtnVolverJu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnVolverJu.BackgroundImage")));
             this.BtnVolverJu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.BtnVolverJu.Location = new System.Drawing.Point(571, 371);
-            this.BtnVolverJu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnVolverJu.Location = new System.Drawing.Point(760, 444);
+            this.BtnVolverJu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnVolverJu.Name = "BtnVolverJu";
-            this.BtnVolverJu.Size = new System.Drawing.Size(140, 42);
+            this.BtnVolverJu.Size = new System.Drawing.Size(187, 52);
             this.BtnVolverJu.TabIndex = 4;
             this.BtnVolverJu.UseVisualStyleBackColor = true;
             this.BtnVolverJu.Click += new System.EventHandler(this.BtnVolverJu_Click);
@@ -91,10 +91,10 @@
             // TxtApo
             // 
             this.TxtApo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtApo.Location = new System.Drawing.Point(293, 27);
-            this.TxtApo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TxtApo.Location = new System.Drawing.Point(391, 33);
+            this.TxtApo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TxtApo.Name = "TxtApo";
-            this.TxtApo.Size = new System.Drawing.Size(154, 28);
+            this.TxtApo.Size = new System.Drawing.Size(204, 34);
             this.TxtApo.TabIndex = 5;
             // 
             // LblNombre
@@ -103,10 +103,9 @@
             this.LblNombre.BackColor = System.Drawing.Color.Transparent;
             this.LblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblNombre.ForeColor = System.Drawing.Color.White;
-            this.LblNombre.Location = new System.Drawing.Point(46, 80);
-            this.LblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LblNombre.Location = new System.Drawing.Point(61, 98);
             this.LblNombre.Name = "LblNombre";
-            this.LblNombre.Size = new System.Drawing.Size(156, 37);
+            this.LblNombre.Size = new System.Drawing.Size(196, 46);
             this.LblNombre.TabIndex = 6;
             this.LblNombre.Text = "NOMBRE";
             // 
@@ -116,10 +115,9 @@
             this.LblApodo.BackColor = System.Drawing.Color.Transparent;
             this.LblApodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblApodo.ForeColor = System.Drawing.Color.White;
-            this.LblApodo.Location = new System.Drawing.Point(286, 80);
-            this.LblApodo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LblApodo.Location = new System.Drawing.Point(381, 98);
             this.LblApodo.Name = "LblApodo";
-            this.LblApodo.Size = new System.Drawing.Size(133, 37);
+            this.LblApodo.Size = new System.Drawing.Size(165, 46);
             this.LblApodo.TabIndex = 7;
             this.LblApodo.Text = "APODO";
             // 
@@ -136,47 +134,52 @@
             this.ColJugadorApodo,
             this.ColJugadorPuntos});
             this.DgvJugadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DgvJugadores.Location = new System.Drawing.Point(53, 146);
-            this.DgvJugadores.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DgvJugadores.Location = new System.Drawing.Point(71, 180);
+            this.DgvJugadores.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DgvJugadores.MultiSelect = false;
             this.DgvJugadores.Name = "DgvJugadores";
             this.DgvJugadores.ReadOnly = true;
             this.DgvJugadores.RowHeadersVisible = false;
+            this.DgvJugadores.RowHeadersWidth = 51;
             this.DgvJugadores.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.DgvJugadores.Size = new System.Drawing.Size(450, 234);
+            this.DgvJugadores.Size = new System.Drawing.Size(600, 288);
             this.DgvJugadores.TabIndex = 8;
             // 
             // ColJugadorId
             // 
             this.ColJugadorId.HeaderText = "ID";
+            this.ColJugadorId.MinimumWidth = 6;
             this.ColJugadorId.Name = "ColJugadorId";
             this.ColJugadorId.ReadOnly = true;
             // 
             // ColJugadorNombre
             // 
             this.ColJugadorNombre.HeaderText = "Nombre";
+            this.ColJugadorNombre.MinimumWidth = 6;
             this.ColJugadorNombre.Name = "ColJugadorNombre";
             this.ColJugadorNombre.ReadOnly = true;
             // 
             // ColJugadorApodo
             // 
             this.ColJugadorApodo.HeaderText = "Apodo";
+            this.ColJugadorApodo.MinimumWidth = 6;
             this.ColJugadorApodo.Name = "ColJugadorApodo";
             this.ColJugadorApodo.ReadOnly = true;
             // 
             // ColJugadorPuntos
             // 
             this.ColJugadorPuntos.HeaderText = "Puntos";
+            this.ColJugadorPuntos.MinimumWidth = 6;
             this.ColJugadorPuntos.Name = "ColJugadorPuntos";
             this.ColJugadorPuntos.ReadOnly = true;
             // 
             // JUGADORES
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(732, 444);
+            this.ClientSize = new System.Drawing.Size(976, 546);
             this.Controls.Add(this.DgvJugadores);
             this.Controls.Add(this.LblApodo);
             this.Controls.Add(this.LblNombre);
@@ -187,7 +190,7 @@
             this.Controls.Add(this.BtnRegistrarJu);
             this.Controls.Add(this.TxtNom);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "JUGADORES";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "JUGADORES";
